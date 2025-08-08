7fa632a6845f9057d0198fcca6f2565bc8d2a6d4
rizkypratama0323 <rizkyfratama.rf3@gmail.com>
2025-08-08 15:08:51 +0800

    Initial commit: Menambahkan file proyek awal

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/', [BookingController::class, 'index']);
+    Route::get('/bookings/create', [BookingController::class, 'create']);
+    Route::post('/bookings', [BookingController::class, 'store']);
+    Route::get('/bookings/{id}/edit', [BookingController::class, 'edit']);
+    Route::put('/bookings/{id}', [BookingController::class, 'update']);
+    Route::delete('/bookings/{id}', [BookingController::class, 'destroy']);
 });
 
 
-Route::get('/', [BookingController::class, 'index']);
-Route::get('/bookings/create', [BookingController::class, 'create']);
-Route::post('/bookings', [BookingController::class, 'store']);
-Route::get('/bookings/{id}/edit', [BookingController::class, 'edit']);
-Route::put('/bookings/{id}', [BookingController::class, 'update']);
-Route::delete('/bookings/{id}', [BookingController::class, 'destroy']);
+
+require __DIR__.'/auth.php';
